efe456bfb44513a3bdf643942f7cfa006fb432cd
electimon <electimon@gmail.com>
2023-03-02 22:26:58 +0800

    kasagi: Use common kernel target
    Signed-off-by: electimon <electimon@gmail.com>

--- device.mk
+++ device.mk
@@ -15,10 +15,6 @@
 # Device path
 DEVICE_PATH := device/motorola/kasagi/rootdir
 
-# Kernel
-PRODUCT_COPY_FILES += \
-    device/motorola/kasagi-kernel/Image.gz:kernel
-
 # Audio Configuration
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/vendor/etc/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml \
