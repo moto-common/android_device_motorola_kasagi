5c57f28e93aea703fe1abe59c7d5bac5b1767257
electimon <electimon@gmail.com>
2023-04-30 01:59:05 +0000

    kasagi: Remove PRODUCT_COPY_FILES
    * All files in rootdir is automatically copied now.
    
    Signed-off-by: electimon <electimon@gmail.com>
    Change-Id: Ia55bf63cbccb39f11076954939d6f97156c5ca6a

--- device.mk
+++ device.mk
@@ -23,26 +23,11 @@ PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 PRODUCT_PROPERTY_OVERRIDES := \
     ro.sf.lcd_density=400
 
-# Audio Configuration
-PRODUCT_COPY_FILES += \
-    $(DEVICE_PATH)/vendor/etc/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml \
-    $(DEVICE_PATH)/vendor/etc/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml \
-    $(DEVICE_PATH)/vendor/etc/audio_policy_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes.xml \
-    $(DEVICE_PATH)/vendor/etc/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml
-
-# Device Init
-PRODUCT_COPY_FILES += \
-    $(DEVICE_PATH)/vendor/etc/init/init.kasagi.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.kasagi.rc
-
 PRODUCT_PACKAGES += \
     fstab.qcom.ramdisk
 
 # Fingerprint
 TARGET_USES_EGISTEC_FINGERPRINT := true
-
-# Media
-PRODUCT_COPY_FILES += \
-    $(DEVICE_PATH)/vendor/etc/media_profiles_vendor.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_vendor.xml
 
 # VBMeta
 TARGET_HAS_VBMETA_SYSTEM := false
